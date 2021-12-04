1a5012da2d9f4b996135694872657efcca1dd8c4
Tim Ruesink <timothy.ruesink@gmail.com>
2021-12-04 10:09:18 -0600

    fixed mobile issues with hero image on main page

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,42 +1,33 @@
 import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
-import {
-  bannerClass,
-  triangle1,
-  triangle2,
-  bannerImage,
-  bannerContent,
-} from './banner.module.scss';
+import { bannerClass, bannerImage, bannerContent } from './banner.module.scss';
 
 const Banner = () => {
   return (
     <div className={bannerClass}>
-      <div className={triangle1}></div>
-      <StaticImage
-        className={bannerImage}
-        imgStyle={{ objectPosition: '20% 0%' }}
-        src="../images/banner1.png"
-        aspectRatio={1}
-        transformOptions={{ cropFocus: 'top' }}
-      />
       <div className={bannerContent}>
-        <div className="text-light">
-          <h1 className="display-1 text-light">TIM RUESINK</h1>
-          <h5 className="fw-light">
-            <span className="fw-light text-secondary">Mechanical Engineer</span>{' '}
-            by Training
-          </h5>
-          <h5 className="fw-light">
-            <span className="fw-light text-secondary">Software Developer</span>{' '}
-            by Passion
-          </h5>
+        <h1 className="display-1 text-light">TIM RUESINK</h1>
+        <h5 className="fw-light text-light">
+          <span className="fw-light text-secondary">Mechanical Engineer</span>{' '}
+          by Training
+        </h5>
+        <h5 className="fw-light text-light">
+          <span className="fw-light text-secondary">Software Developer</span> by
+          Passion
+        </h5>
+        <div>
           <button className="btn btn-secondary me-2 my-2">Contact Me</button>
           <a href="#projects" className="btn btn-outline-light my-2">
             Projects
           </a>
         </div>
       </div>
-      <div className={triangle2}></div>
+      <StaticImage
+        className={bannerImage}
+        src="../images/banner1.png"
+        aspectRatio={1}
+        transformOptions={{ cropFocus: 'top' }}
+      />
     </div>
   );
 };
